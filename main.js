aac30c37b0f2797fd6a7a7dbb594fdc168e6c914
Charles Watson <nchlswtsn@gmail.com>
2015-08-27 17:11:11 -0700

    Tables replaced with divs

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
 	var randomNumber - Math.floor(Math.random() * 9);
-	
-    $( "a" ).mouseenter(function(event) {
-  		$( "a" ).fadeToggle( "slow", 0.25 );
+    $( "#unclicked" ).mouseenter(function(event) {
+  		$( "#unclicked" ).fadeToggle( "slow", 0.25 );
 	});
 });
