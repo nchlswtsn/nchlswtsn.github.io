acb7843edebca7a68c71d1cba6dc7417097b800a
Charles Watson <nchlswtsn@gmail.com>
2015-08-29 10:30:20 -0700

    deleted previous content

--- main.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-	var randomNumber = Math.floor(Math.random() * 9);
-    $( "#unclicked" ).mouseenter(function(event) {
-  		$( "#unclicked" ).fadeToggle( "slow");
-	});
-});
