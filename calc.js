3bf819cad31312f3e9d6f4ed95b1deb1d106c031
Charles Watson <nchlswtsn@gmail.com>
2015-08-29 13:34:10 -0700

    add num limit functionality to js

--- calc.js
+++ calc.js
@@ -1,4 +1,13 @@
 $(document).ready(function(){
+  var testNumLength = function(number) {
+        if (number.length > 9) {
+            totaldiv.text(number.substr(number.length-9,9));
+            if (number.length > 15) {
+                number = "";
+                totaldiv.text("Err");
+            }
+        }
+    };
   var number = "";
   var newnumber = "";
   var operator = "";
